73a203b69eddbc030b0d9527904707bf48d7454a
mac air <mac.air@bla.net>
2011-11-01 00:26:55 +0100

    added scripts directory / updated relink.sh / ads PATH

--- relink.sh
+++ relink.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 HIDDIR=$(pwd)/hiddenfiles
+SCRIPTSDIR=$(pwd)/scripts
 
 echo "Relinking dotfiles in ~/ "
 for file in $(ls $HIDDIR/)
@@ -8,4 +9,15 @@ do
     rm -rf ~/.$file
     ln -s $HIDDIR/$file ~/.$file
 done
+
+echo "Relinking scripts in ~/local/scripts/ "
+if [ ! -d ~/local/scripts ] ; then
+    mkdir -p ~/local/scripts
+fi
+for file in $(ls $SCRIPTSDIR/)
+do
+    rm -rf ~/local/scripts/$file
+    ln -s $SCRIPTSDIR/$file ~/local/scripts/$file
+done
+
 echo "Done."
